66319b6e8a0d9e62fb51624835e109d388077ef5
eunyoung23 <eun970923@naver.com>
2022-12-10 11:35:19 +0900

    Add: 라우터 설정 및 네이버 뉴스 크롤링 관련 파일 추가 (#1)

---
+++ routes/news/naver.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const commonFunc = require('../../common');
+let request = require('request');
+let cheerio = require('cheerio');
+
+module.exports = router;
