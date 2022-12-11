12be8c334d58d9416610cb61202961f0fc14694b
eunyoung23 <eun970923@naver.com>
2022-12-11 11:06:33 +0900

    Add: codingworld.js 파일 추가 및 router 추가 설정(#7)

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const naverRouter = require('./news/naver');
+const codingworldRouter = require('./news/codingworld');
 
 router.use('/naver',naverRouter);
+router.use('/codingworld',codingworldRouter);
 
 module.exports = router;
