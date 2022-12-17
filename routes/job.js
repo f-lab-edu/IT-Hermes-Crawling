423eb1d2ba980e80b0b0e56a07e8fc4bb587b6cb
eunyoung23 <eun970923@naver.com>
2022-12-18 00:22:15 +0900

    Feat: 원티드 크롤링 코드 구현(#11)

--- routes/job.js
+++ routes/job.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const saraminRouter = require('./job/saramin');
+const wantedRouter = require('./job/wanted');
 
 router.use('/saramin', saraminRouter);
+router.use('/wanted',wantedRouter);
 
 module.exports = router;
