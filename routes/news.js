d05b21aafc2017fa0742915513c5b01e0a6c982f
eunyoung23 <eun970923@naver.com>
2023-02-22 18:25:55 +0900

    Fix: 요즘it 더미데이터 코드 수정

--- routes/news.js
+++ routes/news.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const naverRouter = require('./news/naver');
 const codingworldRouter = require('./news/codingworld');
 const yozmRouter = require('./news/yozm');
+const yozmDummyRouter = require('./news/dummyyozm');
 
 router.use('/naver',naverRouter);
 router.use('/codingworld',codingworldRouter);
 router.use('/yozm',yozmRouter);
+router.use('/dummyyozm',yozmDummyRouter);
 
 module.exports = router;
---
+++ routes/news/dummyyozm.js
@@ -0,0 +1,52 @@
+const commonFunc = require('../../common');
+const axios = commonFunc.axios;
+let cheerio = commonFunc.cheerio;
+
+const express = commonFunc.express;
+const router = express.Router();
+
+const defaultUrl = "https://yozm.wishket.com";
+
+router.get('/', (req,res,next) => {
+    let page = req.query.page;
+
+    const requestYozmParameter = () => {
+        return `https://yozm.wishket.com/magazine/list/develop/?page=${page}&q=`;
+    }
+
+    axios(requestYozmParameter())
+         .then(response => res.json(responseYozmData(response.data)))
+         .catch(error => res.json(error));
+})
+
+
+const responseYozmData = (body) => {
+    const $ = cheerio.load(body);
+    let main = $('.item-main');
+    let list = main.find('.item-title');
+    let time = main.find('.content-meta-elem');
+    let descriptList = main.find('.item-description');
+    let imageList = $('.item-thumbnail-pc a img');
+    let newsDateList = [];
+    let crawlingList=[];
+    for(let i=0;i<time.length;i++) {
+        let childData = time[i].children[0];
+        if(!commonFunc.isEmpty(childData)) {
+            if(String(childData.data).includes('전') || String(childData.data).includes('.')) {
+                newsDateList.push(commonFunc.convertTextToDt(childData.data));
+            }
+        }
+    }
+    for(let i=0;i<list.length;i++) {
+        crawlingList.push({
+            title:list[i].children[0].data,
+            description: descriptList[i].children[0].data,
+            thumbnail : defaultUrl+imageList[i].attribs.src,
+            url:defaultUrl+list[i].attribs.href,
+            date:newsDateList[i]
+        })            
+    }
+    return crawlingList;
+}
+
+module.exports = router;
